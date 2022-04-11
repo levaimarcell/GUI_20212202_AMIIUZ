6a1ea3e9a9ba2bd06e5617329cb097098c2f69db
LévaiMarcell <levaimarcell65@gmail.com>
2022-04-11 13:44:41 +0200

    backround image displayed

---
+++ WPF_GunMayhem/Renderer/Display.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WPF_GunMayhem.Renderer
+{
+    internal class Display : FrameworkElement
+    {
+        Size area;
+
+        public void SetupSizes(Size area)
+        {
+            this.area = area;
+            this.InvalidateVisual();
+        }
+        public Brush BackgroundBrush
+        {
+            get
+            {
+                return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "background.png"), UriKind.RelativeOrAbsolute)));
+            }
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            if(area.Width > 0 && area.Height > 0)
+            {
+                drawingContext.DrawRectangle(BackgroundBrush, null, new Rect(0, 0, area.Width, area.Height));
+            }
+        }
+    }
+}
